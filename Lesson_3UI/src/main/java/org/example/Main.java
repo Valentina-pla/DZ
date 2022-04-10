3b7ed8dca2fc31d7c5fc8dce97d3352b1bfeb909
vs_pl <vs_platonova@outlook.com>
2022-04-10 17:04:47 +0300

    Домашнее Задание Урок 4 Junit

---
+++ Lesson_3UI/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+
+    public static void main(String[]args) throws Exception {
+        Triangle tri=new Triangle();
+        float area = tri.checkArea(30,30,10);
+        System.out.println(area);
+
+
+
+    }
+
+}
