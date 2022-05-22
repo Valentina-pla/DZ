ec8e72619521073968e707cc3950f819c3ccda05
vs_pl <vs_platonova@outlook.com>
2022-05-22 13:59:48 +0300

    ю

---
+++ DZ-backend3/test_backend/src/test/java/MealPlan.java
@@ -0,0 +1,57 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class MealPlan {
+
+    String id;
+    @Test
+    void addAndDeleteMealTest() {
+        id = given()
+                .queryParam("hash", "bd709e5bda1165a4b65b87928dad8b58704d7601")
+                .queryParam("apiKey", "f0aff8f0262c4d24a997529555086625")
+                .body("{\n"
+                        + " \"date\": 1644881179,\n"
+                        + " \"slot\": 1,\n"
+                        + " \"position\": 0,\n"
+                        + " \"type\": \"INGREDIENTS\",\n"
+                        + " \"value\": {\n"
+                        + " \"ingredients\": [\n"
+                        + " {\n"
+                        + " \"name\": \"1 banana\"\n"
+                        + " }\n"
+                        + " ]\n"
+                        + " }\n"
+                        + "}")
+
+                .when()
+                .post("https://api.spoonacular.com/mealplanner/vplatonova/items")
+                .then()
+                .statusCode(200)
+                .extract()
+                .jsonPath()
+                .get("id")
+                .toString();
+
+    }
+
+    @AfterEach
+    void tearDown() {
+        given()
+                .queryParam("hash", "bd709e5bda1165a4b65b87928dad8b58704d7601")
+                .queryParam("apiKey", "f0aff8f0262c4d24a997529555086625")
+                .delete("https://api.spoonacular.com/mealplanner/vplatonova/items/" + id)
+                .then()
+                .statusCode(200);
+    }
+
+    
+
+
+
+
+}
